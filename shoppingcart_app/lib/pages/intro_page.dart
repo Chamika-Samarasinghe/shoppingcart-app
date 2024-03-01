dc6aff5bcf0ca1ce7dbbb563d7185bdc82ca2be5
Chamika <chamikasamarasinghe12@gmail.com>
2024-03-01 21:32:22 +0530

    Created the intro page
    Added assets and created the intro page

---
+++ shoppingcart_app/lib/pages/intro_page.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Column(
+            children: [
+
+              //logo
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Image.asset(
+                  'lib/images/nike.png',
+                  height: 300,
+                ),
+              ),
+              const SizedBox(height: 48,),
+
+              //title
+              const Text(
+                'Just Do It',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+              ),
+
+              const SizedBox(height: 24,),
+
+              //sub title
+              const Text(
+                'Brand new sneakers and custom kicks made with premium quality',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey,
+                ),
+                textAlign: TextAlign.center,
+              ),
+
+              const SizedBox(height: 48,),
+
+              //start now button
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.grey[900],
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                padding: const EdgeInsets.all(25),
+                child: Center(
+                  child: const Text(
+                    'Shop Now',
+                    style: TextStyle(color: Colors.white
+                    ),
+                  ),
+                ),
+              ),
+
+            ],
+
+          ),
+        ),
+      ),
+    );
+  }
+}
